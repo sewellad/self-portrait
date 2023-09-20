2b8fa5f262913f9e4d79b7a399d9e59a3f362b57
sewellad <144348593+sewellad@users.noreply.github.com>
2023-09-20 09:03:06 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Subject 468":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f a a f f a a f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f b b f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. f f f f f . f f . f f f f f . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f f f f f f . . . . 
+`;
+            case "image2":
+            case "Adrianna":return img`
+. 1 f f f f f f f f f f f 1 . . 
+. 1 f f f f f f f f f f f 1 . . 
+. 1 f f f f e e e e e f f 1 . . 
+. 1 f f f f f e f f e f f 1 . . 
+. 1 f f f f f e f f e f f 1 . . 
+. 1 f f e e e e e e e f f 1 . . 
+. 1 f f e f e e e f e f f 1 . . 
+. 1 f f e e f f f e e f f 1 . . 
+. . 1 f e e e e e e e f 1 . . . 
+. . . 1 f f f f f f f 1 . . . . 
+. 1 1 1 1 1 f f f 1 1 1 1 1 . . 
+1 f f f f 1 f f f 1 f f f f 1 . 
+. 1 1 1 1 f f f f f 1 1 1 1 . . 
+. . . . . 1 f f f 1 . . . . . . 
+. . . 1 1 f f f f f 1 1 . . . . 
+. . 1 f f f f f f f f f 1 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
